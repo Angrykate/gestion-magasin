75a99402a639eddae2dc1b97dc3a4778e734c714
Angrykate <illanesidibe908@gmail.com>
2026-01-13 00:48:00 +0000

    Lien avec la base

--- db/models.py
+++ db/models.py
@@ -1,10 +1,11 @@
 from db.connection import get_connection, get_cursor
 
 # Exemple minimal pour l'insertion
-def add_utilisateur(nom, email, mot_de_passe, role):
+def add_employee(nom, email, mot_de_passe, role):
     conn = get_connection()
     if not conn:
         return False
+
     cur = get_cursor(conn)
     try:
         cur.execute("""
@@ -14,12 +15,34 @@ def add_utilisateur(nom, email, mot_de_passe, role):
         conn.commit()
         return True
     except Exception as e:
-        print("Erreur add_utilisateur :", e)
+        print("Erreur add_employee :", e)
         conn.rollback()
         return False
     finally:
         cur.close()
         conn.close()
+
+def get_all_employees():
+    conn = get_connection()
+    if not conn:
+        return []
+
+    cur = get_cursor(conn)
+    try:
+        cur.execute("""
+            SELECT id_utilisateur, nom, email, mot_de_passe, role
+            FROM utilisateur
+            WHERE role != 'ADMIN'
+            ORDER BY id_utilisateur DESC
+        """)
+        return cur.fetchall()
+    except Exception as e:
+        print("Erreur get_all_employees :", e)
+        return []
+    finally:
+        cur.close()
+        conn.close()
+
 
 def authenticate_user(email, password):
     conn = get_connection()
